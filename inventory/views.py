b2d378f424d914b7ed2164e5987882dbb9a38519
codebicycle <codebicycle@gmail.com>
2016-11-05 09:23:42 +0200

    Add index, item_detail views

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse('<p>Inventory Index View</p>')
+
+def item_detail(request, id):
+    return HttpResponse('<p>item_detail view with id {}</p>'.format(id))
