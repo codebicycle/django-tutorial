77080600832eaa087d6c270aa723c3219f1dd238
codebicycle <codebicycle@gmail.com>
2016-11-06 19:21:42 +0200

    Model Form with Class-Based Views

--- books/views.py
+++ books/views.py
@@ -2,15 +2,16 @@ from django.db.models import Count
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import DetailView, View
 from .models import Author, Book
-from .forms import ReviewForm
+from .forms import BookForm, ReviewForm
+
 
 def list_books(request):
     """List the books that have reviews
 
     """
     books = Book.objects \
-            .exclude(date_reviewed__isnull=True) \
-            .prefetch_related('authors')
+        .exclude(date_reviewed__isnull=True) \
+        .prefetch_related('authors')
 
     context = {
         'books': books
@@ -19,15 +20,33 @@ def list_books(request):
     return render(request, 'books/index.html', context)
 
 
-def review_books(request):
+class ReviewList(View):
     """List the books that we want to review
 
     """
-    books = Book.objects.filter(date_reviewed__isnull=True).prefetch_related('authors')
-    context = {
-        'books': books,
-    }
-    return render(request, 'books/list-to-review.html', context)
+
+    def get(self, request):
+        books = Book.objects.filter(date_reviewed__isnull=True).prefetch_related('authors')
+        context = {
+            'books': books,
+            'form': BookForm,
+        }
+        return render(request, 'books/list-to-review.html', context)
+
+    def post(self, request):
+        form = BookForm(request.POST)
+        books = Book.objects.filter(date_reviewed__isnull=True).prefetch_related('authors')
+
+        if form.is_valid():
+            form.save()
+            return redirect('review-books')
+
+        context = {
+            'form': form,
+            'books': books,
+        }
+
+        return render(request, 'books/index.html', context)
 
 
 def review_book(request, pk):
@@ -39,7 +58,7 @@ def review_book(request, pk):
         form = ReviewForm(request.POST)
         if form.is_valid():
             book.is_favourite = form.cleaned_data['is_favourite']
-            book.review       = form.cleaned_data['review']
+            book.review = form.cleaned_data['review']
             book.save()
 
             return redirect('review-books')
@@ -55,11 +74,10 @@ def review_book(request, pk):
 
 
 class AuthorList(View):
-
     def get(self, request):
         authors = Author.objects.annotate(
             published_books=Count('books')
-        ). filter(
+        ).filter(
             published_books__gt=0
         )
 
@@ -71,10 +89,10 @@ class AuthorList(View):
 
 
 class BookDetail(DetailView):
-    model         = Book
+    model = Book
     template_name = 'books/book.html'
 
 
 class AuthorDetail(DetailView):
-    model         = Author
+    model = Author
     template_name = 'books/author.html'
