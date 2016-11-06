8a3c6953d9495fbbffc33d5f698cb5651e4b32a4
codebicycle <codebicycle@gmail.com>
2016-11-06 18:28:41 +0200

    Add review form

--- books/urls.py
+++ books/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^authors/$', views.AuthorList.as_view(), name='authors'),
     url(r'^book/(?P<pk>[-\w]+)/$', views.BookDetail.as_view(), name='book-detail'),
     url(r'^author/(?P<pk>[-\w]+)/$', views.AuthorDetail.as_view(), name='author-detail'),
+    url(r'^review/$', views.review_books, name='review-books'),
+    url(r'^review/(?P<pk>[-\w]+)/$', views.review_book, name='review-book'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,8 @@
 from django.db.models import Count
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views.generic import DetailView, View
 from .models import Author, Book
+from .forms import ReviewForm
 
 def list_books(request):
     """List the books that have reviews
@@ -16,6 +17,31 @@ def list_books(request):
     }
 
     return render(request, 'books/index.html', context)
+
+
+def review_books(request):
+    """List the books that we want to review
+
+    """
+    books = Book.objects.filter(date_reviewed__isnull=True).prefetch_related('authors')
+    context = {
+        'books': books,
+    }
+    return render(request, 'books/list-to-review.html', context)
+
+
+def review_book(request, pk):
+    """Review an individual book
+
+    """
+    book = get_object_or_404(Book, pk=pk)
+    form = ReviewForm
+
+    context = {
+        'book': book,
+        'form': form,
+    }
+    return render(request, 'books/review-book.html', context)
 
 
 class AuthorList(View):
