5283ef8d6475eabfad37bc2cc2bfae8c5b6bca43
codebicycle <codebicycle@gmail.com>
2016-11-06 07:06:41 +0200

    Use template views for inventory

--- inventory/views.py
+++ inventory/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+
+from inventory.models import Item
 
 def index(request):
-    return HttpResponse('<p>Inventory Index View</p>')
+    items = Item.objects.exclude(amount=0)
+    return render(request, 'inventory/index.html', {
+        'items': items,
+        })
 
 def item_detail(request, id):
-    return HttpResponse('<p>item_detail view with id {}</p>'.format(id))
+    try:
+        item = Item.objects.get(id=id)
+    except Item.DoesNotExist:
+        raise Http404('This item does not exist')
+
+    return render(request, 'inventory/item_detail.html', {
+        'item': item,
+        })
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from inventory import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='inventory-index'),
     url(r'^item/(?P<id>\d+)/', views.item_detail, name='item_detail'),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
